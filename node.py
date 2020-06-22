3c0537cf1716d779e8f1d6b853355eae9101bc10
Ali Nazari <ali.nazari@aut.ac.ir>
2020-06-23 01:54:37 +0430

    fix tcp socket bug - make tcp sockets single-use

--- node.py
+++ node.py
@@ -10,14 +10,7 @@ class Node:
         
         self.upd_port = port
         self.udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.udp_socket.bind((self.host, self.upd_port))
-
-        self.tcp_port = self.get_free_tcp_port()
-        self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.tcp_socket.bind((self.host, self.tcp_port))
-
-        if port != 6655:
-            self.tcp_socket.listen()
+        self.udp_socket.bind((self.host, self.upd_port))        
 
         self.label = "N" + str(self.upd_port)
         if not os.path.exists(self.label):
@@ -65,16 +58,16 @@ class Node:
                 print("[MESSAGE]: " + "N" + str(address[1]) + " wants " + msg.split()[1] + ".")  
             elif msg.split()[0] == "[MESSAGE]:":
                 print(msg)
-                # print(msg.split()[3])
-                # print(msg.split()[1])
-                # print(msg.split()[-1])
-                tcp_client = threading.Thread(target=self.receive_file, args=(msg.split()[3], int(msg.split()[-1])))
+                tcp_client = threading.Thread(target=self.file_receiver, args=(msg.split()[3], int(msg.split()[-1])))
                 tcp_client.start()
             else:
                 print(msg)
 
             if os.path.isfile('./' + self.label + '/' + msg.split()[1]):
-                self.send_msg("[MESSAGE]: " + self.label + " has " + msg.split()[1] + " and the TCP port is: " + str(self.tcp_port))
+                port = self.get_free_tcp_port()
+                tcp_server = threading.Thread(target=self.file_server, args=(port,))
+                tcp_server.start()
+                self.send_msg("[MESSAGE]: " + self.label + " has " + msg.split()[1] + " and the TCP port is: " + str(port))
  
     def discovery_handler(self):
         pass
@@ -85,10 +78,6 @@ class Node:
 
         udp_server = threading.Thread(target=self.server_handler)
         udp_server.start()
-
-        if self.upd_port != 6655:
-            tcp_server = threading.Thread(target=self.file_server)
-            tcp_server.start()
 
         # server.join()
         # discovery = threading.Thread(target=self.discovery_handler, args=(client, ))
@@ -101,41 +90,42 @@ class Node:
         tmp_socket.close()
         return port
 
-    def file_server(self):
-        while True:
-            connection, address = self.tcp_socket.accept()
-            print(address)
+    def file_server(self, port):
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind((self.host, port))
+        server.listen()
 
-            message_length = int(connection.recv(MESSAGE_LENGTH_SIZE).decode(ENCODING))
-            file_name = connection.recv(message_length).decode(ENCODING)
+        connection, address = server.accept()
+        message_length = int(connection.recv(MESSAGE_LENGTH_SIZE).decode(ENCODING))
+        file_name = connection.recv(message_length).decode(ENCODING)
 
-            print(file_name)
-
-            f = open('./' + self.label + '/' + file_name,'rb')
+        f = open('./' + self.label + '/' + file_name,'rb')
+        l = f.read(MESSAGE_LENGTH_SIZE)
+        while (l):
+            connection.send(l)
             l = f.read(MESSAGE_LENGTH_SIZE)
-            while (l):
-                connection.send(l)
-                l = f.read(MESSAGE_LENGTH_SIZE)
-            f.close()
-            print('Done sending')
-            connection.close()
+        f.close()
+        print('[MESSAGE]: Finish sending ' + file_name + '.')
+        connection.close()
+        server.close()
 
+    def file_receiver(self, file_name, port):
+        receiver = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        receiver.connect((self.host, port))
 
-    def receive_file(self, file_name, port):
-        self.tcp_socket.connect((self.host, port))
         message = file_name.encode(ENCODING)
         msg_length = len(message)
         msg_length = str(msg_length).encode(ENCODING)
         msg_length += b' ' * (MESSAGE_LENGTH_SIZE - len(msg_length))
-        self.tcp_socket.send(msg_length)
-        self.tcp_socket.send(message)
+        receiver.send(msg_length)
+        receiver.send(message)
 
         f = open('./' + self.label + '/' + file_name, 'wb')
         while True:
-            data = self.tcp_socket.recv(MESSAGE_LENGTH_SIZE)
+            data = receiver.recv(MESSAGE_LENGTH_SIZE)
             if not data:
                 break
             f.write(data)
         f.close()
-        print("file finish")
-        self.tcp_socket.close()
+        print('[MESSAGE]: Finish receiving ' + file_name + '.')
+        receiver.close()
